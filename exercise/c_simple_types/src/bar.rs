8851ba0a29d5f6962819022aaddfcbb81b3a98a7
Afr Schoe <58883403+q9f@users.noreply.github.com>
2022-04-01 13:00:04 +0200

    c: simple types

---
+++ exercise/c_simple_types/src/bar.rs
@@ -0,0 +1,36 @@
+/*
+
+scalar types: integers (unsigned, signed), floats (precision), booleans, characters
+
+decimal 10_000
+hex     0xdead_beef
+octal   0o72_345
+binary  0b101_010
+byte    b'A'    (u8!)
+
+float 0.1
+      3.14_f32
+
+bool true, false
+
+char 'a' (single quotes)
+
+*/
+
+/*
+compount types: tuple
+
+let info (u8, f32, i32) = (1, 3.3, 999);
+let jets = info.0;
+let fuel = info.1;
+let ammo = info.2;
+let (jets, fule, ammo) = info;
+// max 12 elements ("aridy"?!)
+
+compount types: array
+let buf = [1, 2, 3];
+let buf = [0; 3]; // ([0, 0, 0])
+let buf: [u8; 3] = [1, 2, 3];
+// max 32 elements
+
+*/
